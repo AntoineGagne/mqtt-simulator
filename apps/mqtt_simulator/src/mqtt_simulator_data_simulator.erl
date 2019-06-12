ae2ade453e720efaab591f5402d035bd2c75e5c3
Antoine Gagné <gagnantoine@gmail.com>
2019-06-12 19:50:26 -0400

    Remove useless code

--- apps/mqtt_simulator/src/mqtt_simulator_data_simulator.erl
+++ apps/mqtt_simulator/src/mqtt_simulator_data_simulator.erl
@@ -9,9 +9,7 @@
 -export([init/1,
          handle_call/3,
          handle_cast/2,
-         handle_info/2,
-         terminate/2,
-         code_change/3]).
+         handle_info/2]).
 
 -define(VIA_GPROC(Id), {via, gproc, {n, l, Id}}).
 
@@ -53,12 +51,6 @@ handle_info({timeout, _Ref, send}, State=#state{values = Values, interval = Inte
 
 handle_info(_Info, State) ->
     {noreply, State}.
-
-terminate(_Reason, _State) ->
-    ok.
-
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
 
 %%%===================================================================
 %%% Internal functions
